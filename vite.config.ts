a3b432e11142f51298642b3a72c4f2286429885a
Guillaume Fradin <guillaumefradin@gmail.com>
2023-11-30 16:58:54 +0000

    add vite-plugin-dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
 // vite.config.js
 import * as path from 'path';
 import {defineConfig} from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
 	build: {
 		lib: {
 			entry: path.resolve(__dirname, 'src/index.ts'),
 			name: 'polygonjs-occlusion',
+			fileName: (format) => `index.${format}.js`,
 		},
 		rollupOptions: {
 			external: ['@polygonjs/polygonjs', 'three'],
 		},
 	},
+	plugins: [
+		dts({
+			insertTypesEntry: true,
+		}),
+	],
 });
